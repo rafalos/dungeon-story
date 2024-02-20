700708743f166b4eefb194a9f3fed3ec6318b4e1
rafalos <rsulkowski1@gmail.com>
2024-02-20 23:23:29 +0100

    set up proxy on dev server to access local api

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,7 @@ import express from 'express';
 import cors from 'cors';
 require('dotenv').config();
 require('express-async-errors');
-import { errorHandler } from './middlewares/error';
 import apiRouter from './routes/api.routes';
-import { notFound } from './middlewares/404';
 
 import path from 'path';
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
