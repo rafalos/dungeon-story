6e0b768ac62bd024635915d5567e6077dbedbe20
rafalos <rsulkowski1@gmail.com>
2024-02-20 21:39:42 +0100

    fix deploy temp workaround

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,6 +12,7 @@ export const app = express();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/game/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter);
 app.use(notFound);
