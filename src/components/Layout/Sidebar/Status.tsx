75a16f53f8664ab1b1deaf1391ebe7b554098f9f
rafalos <rsulkowski1@gmail.com>
2024-02-23 11:39:25 +0100

    add gold amount to status bar

--- src/components/Layout/Sidebar/Status.tsx
+++ src/components/Layout/Sidebar/Status.tsx
@@ -2,6 +2,7 @@ import ResourceBar from '@/components/UI/ResourceBar';
 import SmallLoader from '@/components/UI/SmallLoader';
 import { SiPowerautomate } from 'react-icons/si';
 import { useUserData } from '@/store/user-slice';
+import { GiTwoCoins } from 'react-icons/gi';
 
 const Status = () => {
   const { isLoading, user } = useUserData();
@@ -10,19 +11,25 @@ const Status = () => {
 
   return (
     <div className="flex items-center justify-center gap-1">
-      <div className="size-8 flex items-center justify-center  ring-[0.8px] ring-customRed font-sans">
+      <div className="size-8 flex items-center justify-center  font-sans ring-[0.8px] ring-customRed">
         {user.level}
       </div>
-      <div className="text-md flex flex-1 flex-col text-customYellow h-8">
+      <div className="text-md flex h-8 flex-1 flex-col text-customYellow">
         <ResourceBar
           bgColor="bg-customRed"
           currentResource={user.experience}
           maxResource={user.maxExperience}
         />
-        <div className="flex">
-          {[...Array(user?.energy)].map((element, index) => (
-            <SiPowerautomate key={index} />
-          ))}
+        <div className="flex items-center justify-between">
+          <span className="flex">
+            {[...Array(user?.energy)].map((element, index) => (
+              <SiPowerautomate key={index} />
+            ))}
+          </span>
+          <span className="flex items-center gap-2">
+            <GiTwoCoins />
+            {user.gold}
+          </span>
         </div>
       </div>
     </div>
