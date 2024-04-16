bdfdf2fb2ba7e7b73cf09bf51208765899337376
rafalos <rsulkowski1@gmail.com>
2024-04-16 09:21:03 +0200

    sort display of stories descending

--- src/pages/StoriesPage.tsx
+++ src/pages/StoriesPage.tsx
@@ -16,18 +16,20 @@ const StoriesPage = () => {
   return (
     <Container title="Stories">
       <div className="flex flex-col gap-2">
-        {stories.map((story) => (
-          <Link
-            to={story.id}
-            key={story.id}
-            className="flex gap-4 bg-slate-50/10 p-2 md:p-6 text-sm text-customWhite hover:cursor-pointer hover:bg-slate-50/20 md:text-lg z-10"
-          >
-            <span className="flex w-20 justify-center">
-              {story.createdAt.toLocaleDateString('pl')}
-            </span>{' '}
-            <span className="flex-1">{story.location}</span>
-          </Link>
-        ))}
+        {stories
+          .sort((a, b) => Number(b.createdAt) - Number(a.createdAt))
+          .map((story) => (
+            <Link
+              to={story.id}
+              key={story.id}
+              className="z-10 flex gap-4 bg-slate-50/10 p-2 text-sm text-customWhite hover:cursor-pointer hover:bg-slate-50/20 md:p-6 md:text-lg"
+            >
+              <span className="flex w-20 justify-center">
+                {story.createdAt.toLocaleDateString('pl')}
+              </span>{' '}
+              <span className="flex-1">{story.location}</span>
+            </Link>
+          ))}
       </div>
     </Container>
   );
